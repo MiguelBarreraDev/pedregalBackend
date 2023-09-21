821dcd3e1378f4194491624134159052b131c1d1
MiguelBarreraDev <mbarrera@aynitech.com>
2023-09-21 02:20:59 -0500

    Fix: Generate zip from evidences

--- routes/evidence.router.js
+++ routes/evidence.router.js
@@ -1,57 +1,56 @@
-const express = require('express')
-const router = express.Router()
-const EvidenceSchema = require('../services/evidence.service')
-const service = new EvidenceSchema()
-const fs = require('fs')
-const archiver = require('archiver')
-const path = require('path')
+const express = require('express');
+const router = express.Router();
+const EvidenceSchema = require('../services/evidence.service');
+const evidenceService = new EvidenceSchema();
+const fs = require('fs');
+const archiver = require('archiver');
+const path = require('path');
 // const { QueryTypes } = require('sequelize');
 // const sequelize = require('../libs/sequelize');
 
 router.get('/', async (req, res) => {
   try {
-    const evidence = await service.find()
-    res.json(evidence)
+    const evidence = await evidenceService.find();
+    res.json(evidence);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-})
+});
 
-router.get('/download/:id', async(req, res) => {
-  const id = req.params.id
+router.get('/download/:reportId', async (req, res) => {
+  const { reportId } = req.params;
   try {
-    const data = await service.findById(id)
-    if(!data || data.length === 0) {
-      return res.status(400).send('No se encontró el archivo')
+    const evidences = await evidenceService.findByReportId(reportId);
+
+    if (!evidences || evidences.length === 0) {
+      return res.status(404).send({
+        errors: [
+          `No se encontraron evidencias para el reporte con id ${reportId}`,
+        ],
+      });
     }
-    const filePath = path.join(__dirname, '../Envios')
-    data.forEach((file, index)=>{
-      const filename = file.fileName
-      try {
-        if(fs.existsSync(filePath)){
-          fs.writeFileSync(filePath, file.evidence)
-          console.log('se pudo')
-        } else {
-          console.log('no existe')
-        }
-        // fs.writeFileSync(filePath, file.evidence)
-      } catch (error) {
-        console.error(error)
-      }
-    })
-    // const zip = archiver('zip')
 
+    const archive = archiver('zip', {
+      zlib: { level: 9 },
+    });
 
-    res.setHeader('Content-Disposition', 'attachment; filename=download.zip')
-    // zip.pipe(res)
-    // zip.finalize()
+    res.setHeader('Content-Type', 'application/zip');
+    res.setHeader(
+      'Content-Disposition',
+      `attachment; filename="evidences_report_${reportId}.zip"`
+    );
 
-    // console.log('data', data)
-    res.status(200).json({data})
+    archive.pipe(res);
+
+    evidences.forEach((evidence) => {
+      archive.append(evidence.evidence, { name: evidence.fileName });
+    });
+
+    archive.finalize();
   } catch (error) {
-    console.error(error)
-    res.status(500).json({error})
+    console.error(error);
+    res.status(500).json({ error });
   }
-})
+});
 
-module.exports = router
+module.exports = router;
